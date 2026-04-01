cb757f9e5c364676e41835a2912981ee47a0eca7
ling <843300795@qq.com>
2026-04-01 17:05:27 +0800

    初始化项目

---
+++ app.py
@@ -0,0 +1,22 @@
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+llm = ChatOllama(
+  model="qwen3.5:9b",                  # 改成你的实际模型名
+  base_url="http://192.168.2.204:11434" # 改成局域网服务器IP
+)
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather]
+)
+
+# Run the agent
+result = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+print(result.get("messages")[-1].content)
