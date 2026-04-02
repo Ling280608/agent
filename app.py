a7159e31078d46d69c48563ceabb8391bc0772f7
ling <843300795@qq.com>
2026-04-02 23:54:51 +0800

    更新线上模型配置方案

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+import os
 
 def get_weather(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
-llm = ChatOllama(
-  model="qwen3.5:9b",                  # 改成你的实际模型名
-  base_url="http://192.168.2.207:11434" # 改成局域网服务器IP
-)
+# 本地ollama模型
+# llm = ChatOllama(
+#   model="qwen3.5:9b",                  # 改成你的实际模型名
+#   base_url="http://192.168.2.207:11434" # 改成局域网服务器IP
+# )
+
+llm = ChatOpenAI(
+    model="qwen3.6-plus",          # 按你的服务端实际模型名改
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",              # 你的千问网关（OpenAI兼容）地址，形如 https://xxx/v1
+    api_key=os.getenv("DASHSCOPE_API_KEY")
+    )
 
 agent = create_agent(
     model=llm,
